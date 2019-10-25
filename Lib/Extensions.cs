11d1f755dcb3893c441bb35f074ac18a3cda66d1
Caleb Cohoon <caleb@imap.cc>
2019-10-24 20:48:43 -0700

    Added preview for strings and truncated grade in table.

---
+++ Lib/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace jeanie.Lib
+{
+    public static class Extensions
+    {
+        public static string Preview(this string value, int length)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+
+            if(value.Length > length)
+            {
+                return value.Substring(0, length) + "...";
+            }
+
+            return value;
+        }
+    }
+}
