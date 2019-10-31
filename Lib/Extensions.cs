955fd69e2a08f23b765b874fcab231d4e5b25cbf
Caleb Cohoon <caleb@imap.cc>
2019-10-30 19:03:57 -0700

    Added extension to extract first word from string.

--- Lib/Extensions.cs
+++ Lib/Extensions.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace jeanie.Lib
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Truncate string to specific size with ellipses.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
         public static string Preview(this string value, int length)
         {
             if (string.IsNullOrEmpty(value)) return value;
@@ -17,6 +24,11 @@ namespace jeanie.Lib
             return value;
         }
 
+        /// <summary>
+        /// Returns the name of the enum in string form with spaces.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
         public static string Text(this Enum value)
         {
             return new string(value.ToString()
@@ -25,6 +37,28 @@ namespace jeanie.Lib
                     ? new[] { ' ', c }
                     : new[] { c })
                 .ToArray()).Trim();
+        }
+
+        /// <summary>
+        /// Returns the first word capitalized.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string FirstWord(this string value)
+        {
+            if (string.IsNullOrEmpty(value)) return value;
+
+            var updatedWord = new StringBuilder();
+            var words = value.Split(' ');
+            for (int i = 0; i < words[0].Length; i++)
+            {
+                if (i == 0)
+                    updatedWord.Append(char.ToUpper(words[0][i]));
+                else
+                    updatedWord.Append(char.ToLower(words[0][i]));
+            }
+
+            return updatedWord.ToString();
         }
     }
 }
---
+++ tests/ExtensionsTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using jeanie.Lib;
+using jeanie.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class ExtensionsTest
+    {
+        [TestMethod]
+        public void ItPreviewsStringWithEllipses()
+        {
+            Assert.AreEqual("This is too long".Preview(6), "This i...");
+        }
+
+        [TestMethod]
+        public void ItReturnsNullWhenPreviewingNullString()
+        {
+            string nullstring = null;
+            Assert.AreEqual(nullstring.Preview(4), null);
+        }
+
+        [TestMethod]
+        public void ItCamelCasesTheEnumValue()
+        {
+            Assert.AreEqual(ReservationStatus.ReminderSent.Text(), "Reminder Sent");
+        }
+
+        [TestMethod]
+        public void ItReturnsOnlyFirstWordWithCapitalization()
+        {
+            Assert.AreEqual("biLL Beau".FirstWord(), "Bill");
+        }
+
+        [TestMethod]
+        public void ItReturnsEmptyFirstWordWhenBlank()
+        {
+            Assert.AreEqual("".FirstWord(), "");
+        }
+
+        [TestMethod]
+        public void ItReturnsNullFirstWordWhenNull()
+        {
+            string nullstring = null;
+            Assert.AreEqual(nullstring.FirstWord(), null);
+        }
+    }
+}
