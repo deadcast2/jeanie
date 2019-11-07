38b57d81685a01dccfac2ea355ac4329d74fab10
Caleb Cohoon <caleb@imap.cc>
2019-11-06 21:22:54 -0800

    Removed phone number, make/model and license plate.

---
+++ Migrations/201911070456412_RemovePhoneAndLicenseInfo.cs
@@ -0,0 +1,22 @@
+namespace jeanie.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RemovePhoneAndLicenseInfo : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.Reservations", "PhoneNumber");
+            DropColumn("dbo.Reservations", "LicensePlateNumber");
+            DropColumn("dbo.Reservations", "MakeAndModel");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.Reservations", "MakeAndModel", c => c.String(maxLength: 100));
+            AddColumn("dbo.Reservations", "LicensePlateNumber", c => c.String(maxLength: 8));
+            AddColumn("dbo.Reservations", "PhoneNumber", c => c.String(maxLength: 24));
+        }
+    }
+}
