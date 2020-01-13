1f5ff1d9735d40f96b728ffebab95d2b88357bec
Caleb Cohoon <caleb@imap.cc>
2020-01-13 11:09:02 -0800

    Updated config with new domain name.

--- Jobs/ReservationReminder.rb
+++ Jobs/ReservationReminder.rb
@@ -4,7 +4,7 @@ require 'uri'
 
 def lambda_handler(event:, context:)
     Net::HTTP.post(
-        URI('https://jeanie-reservation-system.azurewebsites.net/Jobs/SendReminders'),
+        URI('https://jeanie-reservation-system.com/Jobs/SendReminders'),
         {}.to_json
     )
 end
