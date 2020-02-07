187dc0b52d3b60ed900e86a731402874c7a3f80d
Caleb Cohoon <caleb@imap.cc>
2020-02-06 21:50:12 -0800

    Action buttons in admin reservations grid now uses icons, Reservations grid init with DataTable.

---
+++ Scripts/app/admin/reservation.js
@@ -0,0 +1,9 @@
+﻿window.JRS = window.JRS || {};
+window.JRS.Admin = window.JRS.Admin || {};
+window.JRS.Admin.Reservation = {};
+
+(function ($, self, undefined) {
+    self.init = function () {
+        $('#reservations_grid').DataTable();
+    };
+})(jQuery, window.JRS.Admin.Reservation);
